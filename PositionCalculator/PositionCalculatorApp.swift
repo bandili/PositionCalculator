1b2ee9e4ec407a9b38ef64440057b6d243be77cb
lisq <lishiquann@gmail.com>
2025-02-28 21:26:14 +0800

    Initial Commit

---
+++ PositionCalculator/PositionCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositionCalculatorApp.swift
+//  PositionCalculator
+//
+//  Created by Bandi Li on 28.02.25.
+//
+
+import SwiftUI
+
+@main
+struct PositionCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PositionCalculatorTests/PositionCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PositionCalculatorTests.swift
+//  PositionCalculatorTests
+//
+//  Created by Bandi Li on 28.02.25.
+//
+
+import Testing
+@testable import PositionCalculator
+
+struct PositionCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
